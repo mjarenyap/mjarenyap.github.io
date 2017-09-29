589063bd413d6914388d6f3cd0b4346ce545a0a4
MJ Yap <mikejarenbyap@Mjarens-Macbook.local>
2017-09-30 01:31:27 +0800

    Committing my official porfolio site

---
+++ js/mobile.js
@@ -0,0 +1,12 @@
+/* mobile handler */
+
+$(document).ready(){
+    
+    function checkWidth(){
+        var windowWidth = $(window).width();
+        
+        if(windowWidth <= 1160){
+            
+        }
+    }
+}
---
+++ js/projects.js
@@ -0,0 +1,7 @@
+/* Javacript handler for projects */
+
+$(document).ready(function(){
+    var projectWrappers = $('div.project-wrapper');
+    
+    $(projectWrappers[projectWrappers.length - 1]).css('border-bottom', 'none');
+});
